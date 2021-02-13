357ae9a8a884b80b648cce0b32381d5d26229027
Vicente <vicantser@yahoo.es>
2021-02-13 20:45:30 +0100

    Versión refactorizada

--- geometria/src/FiguraGeometrica_VAS.java
+++ geometria/src/FiguraGeometrica_VAS.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_VAS {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_VAS(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
